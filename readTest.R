68e6ec12ebc6f406c7f421afbdd7d00a84ff46c4
e23099 <e23099@gmail.com>
2018-09-07 16:44:33 +0800

    讓 CollecAll() 可抓取異常工作表。自變數sheet.name改成欲讀取的年級(grade)和學期(semester)

--- readTest.R
+++ readTest.R
@@ -37,7 +37,37 @@ getControlSchool = function(path, type){
     school.control
 }
 
-CollectAll = function(x, DataDir, sheet.name, xlsx.name, type){
+getAllSheet = function(school, path){
+    if(!grepl(".xlsx", school))
+        school = paste0(school, ".xlsx")
+    allSheet = excel_sheets(paste0(path, "/", school))
+    return(allSheet)
+}
+
+getSheet = function(xlsx, path, grades, semester){
+    if(!grepl(".xlsx", xlsx))
+        xlsx = paste0(xlsx, ".xlsx")
+    allSheet = try(excel_sheets(paste0(path, "/", xlsx)))
+    if(class(allSheet) == "try-error"){
+        print("wrong path or xlsx name")
+        return(NULL)
+    }
+    grade = paste0(grades, collapse = "")
+    allSheet = gsub(" ", "", allSheet, fixed = TRUE)
+    sheet.toRead = grepl(paste0("[", grade, "]年級$"), allSheet) & grepl(semester, allSheet)
+    return(allSheet[sheet.toRead])
+}
+
+tryCollect = function(x, path, grade, semester, xlsx.name){
+    for(i in 1:length(x)){
+        allsheets = getSheet(xlsx.name[i], path, grade, semester)
+        for(j in allsheets){
+            print(j)
+        }
+    }
+}
+
+CollectAll = function(x, DataDir, grade, semester, xlsx.name, type){
     ## init
     if(type == "junior"){
         parts = rep(1:5,c(3,3,3,6,9)) # 各大題題數：1~5大題，分別有 3 3 3 6 9 題
@@ -78,10 +108,9 @@ CollectAll = function(x, DataDir, sheet.name, xlsx.name, type){
     ## Data Collecting
     JuniorAll = c()
     for(i in 1:length(x)){
-        # get proper sheet.name in this school
-        # sheet.name = try(excel_sheets(paste0(DataDir,"/", xlsx.name[i])))
-        # if(class(sheet.name) == "try-error") print(...)
-        # sheet.name = sheet.name[getSheet(v, grade, semes)]
+        
+        sheet.name = getSheet(xlsx.name[i], DataDir, grade, semester)
+        
         for(j in 1:length(sheet.name)){
             # get number of rows in a xlsx file
             n = try(nrow(read_excel(paste0(DataDir,"/",xlsx.name[i],".xlsx"),
