2aa8df34ed3cc0ae2ec774ea94f9aeea87228f21
e23099 <e23099@gmail.com>
2018-09-16 00:21:34 +0800

    新增信效度分析中以概念區分題組的功能CreateReva_Concept()

--- Reliability_Validity.R
+++ Reliability_Validity.R
@@ -22,11 +22,7 @@ CreateReva = function(RealAns, type, fm = "gls", tableName = "Table 5 Reliabilit
         ## step 1: remove duplicated/useless columns in this part
         qus = ans[,which(part == i)]                        # obtain questions in this part
         DP_qus = which(duplicated(lapply(qus, c)) == TRUE)  # get questions which have the same result
-        NG_qus = c()                                        # get questions which have homogenious result(all 1 or 0s)
-        for(j in 1:ncol(qus)){
-            if(length(unique(na.exclude(qus[,j]))) < 2)
-                NG_qus = append(NG_qus, j)
-        }
+        NG_qus = which(lapply(qus, function(x)length(unique(na.exclude(x)))) < 2)  # get questions which have homogenious result(all 1 or 0s)
         
         bad_qus = c(DP_qus, NG_qus)
         if(length(bad_qus)){
@@ -68,4 +64,70 @@ CreateReva = function(RealAns, type, fm = "gls", tableName = "Table 5 Reliabilit
     return(ReVa)
 }
 
-
+CreateReva_Concept = function(RealAns, type, fm = "gls", tableName = "Table 5 Reliability and Validity (Concept)"){
+  ## init
+    ans = RealAns[,5:ncol(RealAns)]
+  ## type setting
+    if(type == "junior"){
+        concept = rep(1:2, c(9,15))
+        part.name = c("L1", "L2", "L3", "R1", "R2")
+        part.num = c(3,3,3,6,9)
+        part = rep(1:5, part.num)
+    }
+    else if(type == "elementary"){
+        concept = rep(1:3, c(30, 20, 10))
+        part.name = paste0("Part", 1:10)
+        part.num = c(5,5,10,10,5,5,5,5,5,5)
+        part = rep(1:10, part.num)
+    }
+    else{
+        print("Not supported type for Reliability and Validity table")
+        return(NULL)
+    }
+  ## Calculate Reliability and Validity for each part
+    Reva = c()
+    for(i in 1:length(unique(concept))){
+        # step 1: obtain qus and clean it
+        qus = ans[,which(concept == i)]
+        DP_qus = which(duplicated(lapply(qus, c)) == TRUE)
+        NG_qus = which(lapply(qus, function(x)length(unique(na.exclude(x)))) < 2)
+        bad_qus = c(DP_qus, NG_qus)
+        if(length(bad_qus)){
+            qus = qus[, -bad_qus]
+            cat("\n")
+            if(length(DP_qus)){
+                print(paste(c(paste("Concept", i, "Duplicated:"), DP_qus), collapse=" "))
+            }
+            if(length(NG_qus)){
+                print(paste(c(paste("Concept", i, "Useless:   "), NG_qus), collapse=" "))
+            }
+        }
+        
+        # step 2: start ItemAnalysis
+        scale = Scale(qus)
+        pre = suppressWarnings(PreProc(na.exclude(scale)))
+        rel = suppressWarnings(ItemAnalysis(pre, fm = fm))
+        
+        # step 3: summarize Alpha and factor loadings in this Concept
+        v = c() # factor loading of each questions in this concept
+        for(k in 1:sum(concept == i)){
+            if(k %in% unique(bad_qus))
+                v = append(v, NA)
+            else{
+                v = append(v, rel$valid$loadings[1])
+                rel$valid$loadings = rel$valid$loadings[-1]
+            }
+        }
+        u = split(v, part[which(concept == i)]) # split v into each part of this concept
+        for(p in 1:length(unique(part[which(concept == i)]))){
+            if(length(u[[p]]) < max(part.num))
+                u[[p]] = append(u[[p]], rep(NA, 10-length(u[[p]])))
+            Reva = rbind(Reva, append(as.numeric(rel$rely$alpha$total[1]), u[[p]]))
+        }
+    }
+    Reva = as.data.frame(Reva)
+    names(Reva) = c("Alpha", paste0("Q", 1:max(part.num)))
+    row.names(Reva) = part.name    
+    write.csv(Reva, paste0(tableName, ".csv"))
+    return(Reva)
+}
