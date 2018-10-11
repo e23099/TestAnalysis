3d804989637ad1cde3a2fe918dfd68cccf5bedc7
e23099 <e23099@gmail.com>
2018-10-11 14:37:58 +0800

    新增 t-test、答對率直方圖 source檔

---
+++ comparison.R
@@ -0,0 +1,120 @@
+print("Should have sourced readTest.R")
+dir = getwd()
+dir.create(paste0(dir,"/compare"), showWarnings = FALSE)
+##
+# input two data frame, with ID on the first column
+# return a list containing 2 dataframe: old, and new
+##
+Pairing = function(new, old){
+    v = intersect(new[,1], old[,1])
+    new = new[which(new[,1]%in%v),]
+    old = old[which(old[,1]%in%v),]
+    return(list(id = v, school = new$"school",new = new, old = old))
+}
+
+##
+# input two vector of the same length, 
+# return "df", "t", "p-value", "mean difference"
+##
+PairTtest = function(new, old, paired = T){
+    comparison = try(t.test(new, old, paired = paired), silent = T)
+    if(class(comparison) == "try-error"){
+        print("t.test fails")
+        return(NULL)
+    }
+    return(c(comparison[[2]],comparison[[1]],comparison[[3]],comparison[[5]]))
+}
+
+##
+# input two dataframe, type of school, whether using paired sample t test
+# return t test result of each school's total accuracy
+##
+CompareAcu = function(new, old, type, paired = T){
+    out = NULL
+    if(paired){
+       com = Pairing(new,old)
+       newAcu = CreateAcu(com$new, type)
+       oldAcu = CreateAcu(com$old, type)
+       if(type == "junior"){
+           newAcu = Junior_weight_correct(newAcu)
+           oldAcu = Junior_weight_correct(oldAcu)
+       }
+       for(school in unique(com$school)){
+           students = which(new$school == school)
+           total = ncol(newAcu)
+           out = rbind(out, c(school,PairTtest(newAcu[students,total], oldAcu[students,total])))
+       }
+    }
+    else{
+        newAcu = CreateAcu(com$new, type)
+        oldAcu = CreateAcu(com$old, type)
+        if(type == "junior"){
+            newAcu = Junior_weight_correct(newAcu)
+            oldAcu = Junior_weight_correct(oldAcu)
+        }
+        for(school in unique(new$school)){
+            students = which(new$school == school)
+            total = ncol(newAcu)
+            out = rbind(out, c(school,PairTtest(newAcu[students,total], oldAcu[students,total])))
+        }
+    }
+    write.csv(out,"Table 6 (Paired) T test.csv",row.names = F)
+    return(out)
+}
+
+
+##
+# intput : two vectors of accuracy
+# return : Paint Comparison histogram (old in gray, new in white, lightgray is overlap)
+##
+HistCompareAcu = function(new, old, slice, main, xlab, ylab){
+    if(max(new,na.rm=T)>1 | max(old,na.rm=T)>1){
+        print("accuracy > 1")
+        return(NULL)
+    }
+    freq_new = max(hist(new, breaks = seq(0,1,by=slice),plot = FALSE)$counts)
+    freq_old = max(hist(old, breaks = seq(0,1,by=slice),plot = FALSE)$counts)
+    
+    hist(old, breaks = seq(0,1,by=.2),col=rgb(0,0,0,0.5), main=main, xlab=xlab, ylab=ylab, ylim=c(0,max(freq_old,freq_new)))
+    hist(new, breaks = seq(0,1,by=.2),col=rgb(1,1,1,0.5), add=T)
+    box()
+}
+
+
+##
+# input  : 2 dataframe, school type, test time for legend names, k plots a row, width, height, slice for histogram
+# return : plot each part's accuracy histogram for each school 
+##
+
+PlotComparison = function(new, old, type, legendName,k, w, h, slice = 0.2){
+    com = Pairing(new,old)
+    newAcu = CreateAcu(com$new, type)
+    oldAcu = CreateAcu(com$old, type)
+    part.name = c(paste0("第",1:10,"部分\n答對率"), "總答對率")
+    if(type == "junior"){
+        part.name = paste0("國中",c("聽力一答對率", "聽力二答對率", "聽力三答對率", "閱讀一答對率", "閱讀二答對率", "總答對率"))
+        newAcu = Junior_weight_correct(newAcu)
+        oldAcu = Junior_weight_correct(oldAcu)
+    }
+    for(i in 1:length(unique(com$school))){
+        plt_count = 0
+        students = which(new$school == unique(com$school)[i])
+        dir.create(paste0(dir,paste0("/compare/school",i)), showWarnings = FALSE)
+        for(j in 1:length(newAcu)){
+            if(plt_count %% k == 0){
+                png(file = paste0(dir, "/compare/school",i,"/",j,".png"), width = w, height = h)
+                par(mfrow=c(1,k), cex.main = 2, cex.lab = 1.5, cex.axis = 1.5)
+            }
+            HistCompareAcu(
+                newAcu[students,j], oldAcu[students,j],
+                main = paste0(unique(com$school)[i],part.name[j],"盒鬚圖"),
+                xlab = "答對率", ylab = "人數"
+            )
+            legend("topleft", c(legendName,"重疊"), 
+                    fill=c("gray46","white","gray"), cex=1.5,  horiz=T, bty="n")
+            plt_count = plt_count+1
+            if(plt_count %% k == 0) dev.off()
+        }
+        try(dev.off(), silent = T) # close last png file if necessary
+    }
+}
